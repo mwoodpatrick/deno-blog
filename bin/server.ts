4e7cedfd8f56699353b53121510a40b8c8a9de93
Mark L. Wood-Patrick <mwoodpatrick@gmail.com>
2020-07-30 16:26:42 -0700

    Add optional https support

--- bin/server.ts
+++ bin/server.ts
@@ -1,6 +1,22 @@
 import app from "../app.ts";
 
-const port = 3000;
+const env = Deno.env.toObject();
 
-console.log(`app running -> http://127.0.0.1:${port}`);
-await app.listen({ port });
+const HOST = env.HOST||"127.0.0.1";
+const PORT  = Number(env.PORT) || 3000;
+const SECURE = !!env.SECURE||false;
+const CERT_FILE = env.CERT_FILE||"";
+const KEY_FILE = env.KEY_FILE||"";
+const PROTOCOL = SECURE ? "https" : "http";
+
+const listenOptions = {
+    port: Number(PORT),
+    secure: SECURE, 
+    certFile : CERT_FILE, 
+    keyFile: KEY_FILE
+};
+
+console.log(`app running -> ${PROTOCOL}://${HOST}:${PORT}`);
+console.dir(listenOptions);
+
+await app.listen(listenOptions);
